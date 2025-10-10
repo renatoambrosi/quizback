b8163bdaacc935d6978374f442696312fc0bb099
renatoambrosi <renato.seragi@gmail.com>
2025-10-10 12:27:22 -0300

    Update tally-sync.js

--- tally-sync.js
+++ tally-sync.js
@@ -5,7 +5,7 @@ class TallySync {
     constructor() {
         this.supabase = createClient(
             process.env.SUPABASE_URL,
-            process.env.SUPABASE_ANON_KEY
+            process.env.SUPABASE_SERVICE_KEY
         );
         
         this.sheetId = process.env.TALLY_SHEET_ID;
