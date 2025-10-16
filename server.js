7d10e208e0a17fba6cb8c5aaff3fa6c46dc04673
renatoambrosi <renato.seragi@gmail.com>
2025-10-16 16:42:16 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const paymentRoutes = require('./routes/payments');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 // ============================================
 // MIDDLEWARES DE SEGURANÇA
 // ============================================
